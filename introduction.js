e475524045e1a028f3eaf14b7e7b48013656ef56
hotgrandma69 <vlang@knowre.com>
2016-05-17 17:30:43 +0900

    first commit

---
+++ introduction.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+//
+//Displayable Name of your script 
+// @name           FirstScriptTesting
+// @description		An Introduction. And...um...ballsacks.
+// @require 		https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js 
+// ==/UserScript==
+
+$(function(){
+	window.alert("BALLSACKS");
+});
